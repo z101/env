def3575131355fe8bfd74262704abf305115d7c5
Zen Devergo <z101.1100@gmail.com>
2017-07-19 13:59:42 +0000

    as final commit

--- as/as.db.py
+++ as/as.db.py
@@ -10,21 +10,19 @@ from aslib import asdbr
 from aslib import asdbw
 from aslib import asdbpath
 
-def clean(text):
-    return re.compile(u'\s\s+', flags = re.U).sub(
-        ' ',
-        re.compile(u'<.*?>', flags = re.U).sub(
-            '',
-            text.replace(u'<br/>', u'; ').replace(u'<br />', u'; ').replace(u'\r', u'').replace(u'\n', u'').replace(u'&nbsp;', ' ')
-        )
-    ).replace(u' ;', u';').strip()
+fnum = 0
 
-def asdata(rurl):
-    def asdatar(rurl, asjs, rcat = None):
-        if (rcat is not None):
-            print u'getting "{}" sub-categories'.format(rcat)
-        else:
-            print u'getting root categories'
+def asdata(path, title, rurl):
+    def clean(text):
+        return re.compile(u'\s\s+', flags = re.U).sub(
+            ' ',
+            re.compile(u'<.*?>', flags = re.U).sub(
+                '',
+                text.replace(u'<br/>', u'; ').replace(u'<br />', u'; ').replace(u'\r', u'').replace(u'\n', u'').replace(u'&nbsp;', ' ')
+            )
+        ).replace(u' ;', u';').strip()
+    def asdatar(path, rurl):
+        global fnum
         html = ashtml(rurl)
         r = u"""
 \s+<td[^>]+>	
@@ -39,22 +37,93 @@ def asdata(rurl):
 """.replace('\n', '\\n')
         ascats = asre(r, html)
         if len(ascats) == 0:
-            pass
+            asnop = asre(u'В данной категории нет товаров', html)
+            if len(asnop) > 0:
+                print u'  !!! WARNING: no products by link "{}"'.format(rurl)
+                return
+            asnav = asre(u'<span class="breadcrumbs pathway">.+?</span>', html)
+            if len(asnav) == 0: raise Exception('breadcrumbs span not found')
+            asnav = asnav[0]
+            aslinks = asre(u'<a href="(?P<url>[^"]+)"[^>]+>\s*(?P<name>[^>]+)\s*</a>', asnav)
+            if len(aslinks) == 0: raise Exception('breadcrumb links not found')
+            asjs = {
+                'cats': [],
+                'prods': [],
+            }
+            for l in aslinks:
+                if 'almerashop' in l['url'].lower():
+                    url = l['url']
+                else:
+                    url = u'http://www.almerashop.ru{}'.format(l['url'])
+                asjs['cats'].append({
+                    'name': l['name'].strip(),
+                    'url': url
+                })
+            ashdr = asre(u'<td[^>]+class="contentheading">(?P<name>[^>]+)</td>', html)
+            if len(ashdr) == 0: raise Exception('header td not found')
+            ashdr = ashdr[0]
+            asjs['cats'].append({
+                'name': ashdr['name'].strip(),
+                'url': rurl
+            })
+            rcat = u' / '.join(c['name'] for c in asjs['cats']) 
+            print u'getting products: [{}] "{}"'.format(str(fnum).zfill(4), rcat)
+            html = ashtml(rurl)
+            asplist = asre(u'<div id="product_list"', html)
+            if len(asplist) == 0: raise Exception('products div not found')
+            asplist = asre(u'<div id="product_list">[^<]*</div>', html)
+            if len(asplist) > 0:
+                print u'  !!! WARNING: empty category "{}"'.format(rcat)
+                return
+            html = ashtml('{}?limit=1000000&limitstart=0'.format(rurl))
+            r = u"""<tr>
+<td[^>]+><div class="product_name"[^>]+><a[^>]+href="(?P<url>[^"]+)">(?P<title>[^<]+)</a></div></td>
+\s*<td[^>]+><div class="manufacturer_link"[^>]+>(?P<man>[^<]+)</div></td>
+\s*<td[^>]+><div class="product_sku"[^>]+>(?P<sku>[^<]+)</div></td>
+\s*<td[^>]+>(\s*<img[^>]+alt="(?P<mark>[^"]+)"[^>]+>\s*)?</td>
+\s*<td[^>]+><div class="product_price"[^>]+>(?P<hprice>.+?)</div></td>
+\s*<td[^>]+>(
+\s*<form[^>]+>.+?</form>
+)?\s*</td>
+\s*<td><img src="(?P<img>[^"]+)"[^>]+></td>		
+\s*</tr>""".replace('\n', '\\n')
+            asprods = asre(r, html)
+            if len(asprods) == 0: raise Exception('products not found')
+            r = u'Результаты\s+(?P<from>\d+)\s+-\s+(?P<to>\d+)\s+из\s+(?P<all>\d+)'
+            ascount = asre(r, html)
+            if len(ascount) == 0:
+                print u'  !!! WARNING: counters not found in "{}"'.format(rcat)
+            else:
+                ascount = ascount[0]
+                expc = int(ascount['all'])
+                if len(asprods) != expc: raise Exception('expected {} product(s), but found {}'.format(expc, len(asprods)))
+            for p in asprods:
+                asjs['prods'].append({
+                    'url': p['url'],
+                    'name': p['title'],
+                    'man': p['man'],
+                    'sku': p['sku'],
+                    'mark': p['mark'],
+                    'price': clean(p['hprice']),
+                    'img': p['img'],
+                })
+            asdbw('{}/{}'.format(path, fnum), asjdump(asjs))
+            fnum += 1
         else:
             for cat in ascats:
-                asjs.append({
-                    'url': cat['url'],
-                    'name': clean(cat['hname']),
-                    'descr': clean(cat['hdescr']),
-                    'cats': []
-                })
-                if (rcat is not None):
-                    ncat = u'{} / {}'.format(rcat, asjs[-1]['name'])
-                else:
-                    ncat = asjs[-1]['name']
-                asdatar('http://www.almerashop.ru{}'.format(cat['url']), asjs[-1]['cats'], ncat)
-    asjs = []
-    asdatar(rurl, asjs)
+                asdatar(path, 'http://www.almerashop.ru{}'.format(cat['url']))
+    global fnum
+    fnum = 0
+    hfpath = asdbpath(path)
+    if os.path.isdir(hfpath) and os.path.exists(hfpath):
+        print 'removing directory {}'.format(hfpath)
+        shutil.rmtree(hfpath)
+    print u'searching products: {}'.format(title)
+    asdatar(path, rurl)
+    print u'done.'
 
-asdata('http://www.almerashop.ru/katalog-tovarov.html')
+asdata('nac', u'Nissan Almera Classic', 'http://www.almerashop.ru/146-nissan-almera-classic.html')
+asdata('amo', u'Амортизаторы KYB', 'http://www.almerashop.ru/278-amortizatory-kyb.html')
+asdata('liq', u'Жидкости', 'http://www.almerashop.ru/391-zhidkosti.html')
 
+#asdata('test', u'Test', 'http://www.almerashop.ru/432-shiny/diski.html')
--- as/as.swerc.py
+++ as/as.swerc.py
@@ -6,20 +6,76 @@ from aslib import asdbpath
 from aslib import asdbr
 from aslib import asdbw
 
+pcount = 0
+
 def aspage(pn, hn, fltfirst = None, srt = None, fltlast = None):
     def cut(s):
         c = 2000
         return (s[:(c - 3)] + '...') if len(s) > c else s
 
+    def wdata(f, path, ps):
+        global pcount
+        for i, pf in enumerate(ps):
+            pj = json.loads(asdbr('{}/{}'.format(path, pf)))
+            pth = u'&nbsp;/&nbsp;'.join(u'<a target="_blank" href="{}">{}</a>'.format(c['url'], c['name']) for c in pj['cats'])
+            for i, p in enumerate(pj['prods']):
+                pcount += 1
+                f.write(u"""
+    <tr>
+        <td>{i}</td>
+        <td>{code}</td>
+        <td><img src="{img}" /></td>
+        <td><a target="_blank" href="{url}">{name}</a></td>
+%
+        <td>{man}</td>
+%
+        <td>{sku}</td>
+%
+        <td>{mark}</td>
+%
+        <td>{price}</td>
+%
+        <td>{path}</td>
+%
+    </tr>
+""".format(
+    i = pcount,
+    code = '{}.{}.{}'.format(path, pf, i),
+    img = p['img'].replace('components/com_virtuemart/shop_image/product/Pirelli_Winter_I_5448bb34630ac.jpg', 'components/com_virtuemart/shop_image/product/resized/Pirelli_Winter_I_5448b443efc98_117x124.jpg'),
+    url = u'http://www.almerashop{}'.format(p['url']),
+    name = p['name'],
+    man = p['man'],
+    sku = p['sku'],
+    mark = p['mark'] if p['mark'] is not None else '',
+    price = p['price'],
+    path = pth
+))
+
     print u'generaring page "{}" - {}'.format(pn, hn)
-    jsonpath = 'usrs.rzn.json'
-    us = os.listdir(asdbpath(jsonpath))
+    jnacpath = 'nac'
+    jamopath = 'amo'
+    jliqpath = 'liq'
+    nacps = []
+    if os.path.isdir(asdbpath(jnacpath)) and os.path.exists(asdbpath(jnacpath)): nacps = [int(fn) for fn in  os.listdir(asdbpath(jnacpath))]
+    nacps.sort()
+    amops = []
+    if os.path.isdir(asdbpath(jamopath)) and os.path.exists(asdbpath(jamopath)): amops = [int(fn) for fn in  os.listdir(asdbpath(jamopath))]
+    amops.sort()
+    liqps = []
+    if os.path.isdir(asdbpath(jliqpath)) and os.path.exists(asdbpath(jliqpath)): liqps = [int(fn) for fn in  os.listdir(asdbpath(jliqpath))]
+    liqps.sort()
     if fltfirst is not None:
-        us = fltfirst(us)
+        nacps = fltfirst(nacps)
+        amops = fltfirst(amops)
+        liqps = fltfirst(liqps)
     if srt is not None:
-        srt(us)
+        srt(nacps)
+        srt(amops)
+        srt(liqps)
     if fltlast is not None:
-        us = fltlast(us)
+        nacps = fltlast(nacps)
+        amops = fltlast(amops)
+        liqps = fltlast(liqps)
     asdir = '/home/z101/repos/swerc/sites/default/as'
     if not os.path.exists(asdir):
         os.makedirs(asdir)
@@ -30,169 +86,22 @@ def aspage(pn, hn, fltfirst = None, srt = None, fltlast = None):
 <table>
     <tr>
         <th>#</th>
-        <th>ava</th>
+        <th>code</th>
         <th>img</th>
-        <th>usr</th>
-        <th>descr</th>
-        <th>about</th>
-        <th>stats</th>
-        <th>rdate</th>
-        <th>cnt</th>
-        <th>cars</th>
-        <th>cmu</th>
+        <th>prod</th>
+        <th>man</th>
+        <th>sku</th>
+        <th>mark</th>
+        <th>price</th>
+        <th>path</th>
     </tr>
 %
 """.format(header = hn))
-        for i, u in enumerate(us):
-            u = json.loads(asdbr('usrs.rzn.json/{}'.format(u)))
-            f.write(u"""
-    <tr>
-        <td>{i}</td>
-        <td><img src="{aimg}" /></td>
-        <td><img src="{img}" /></td>
-        <td><a target="_blank" href="https://www.drive2.ru/users/{usr}">{nameu}</a></td>
-%
-        <td>{descr}</td>
-%
-        <td>{about}</td>
-%
-        <td>{stats}</td>
-%
-        <td>{rdate}</td>
-        <td>
-            B&nbsp;{cblog}&nbsp;<br />
-            F&nbsp;<span style="color:red;">{cfollowers}</span>&nbsp;<br />
-            C&nbsp;{ccfollowing}&nbsp;<br />
-            U&nbsp;{cfollowing}&nbsp;<br />
-        </td>
-%
-        <td>{cars}</td>
-%
-        <td>{cmu}</td>
-%
-    </tr>
-""".format(
-    i = (i + 1),
-    aimg = u['aimg'].replace('960.jpg', '120.jpg'),
-    img = u['img'].replace('200.jpg', '100.jpg'),
-    usr = u['usr'],
-    nameu = u['nameu'],
-    descr = cut(u['descr']),
-    about = cut(u['about']),
-    stats = cut(u['stats']),
-    rdate = u['rdate'].replace('-', '&#8209;'),
-    cblog = u['counters']['blog'],
-    cfollowers = u['counters']['followers'],
-    ccfollowing = u['counters']['carfollowing'],
-    cfollowing = u['counters']['following'],
-    cars = u"""
-<table style="border:0">
-{}
-</table>
-""".format(''.join(u"""
-<tr>
-    <td style="border:0"><img src="{img}" /></td>
-    <td style="border:0"><a {former} target="_blank" href="https://www.drive2.ru{url}">{name}</a></td>
-    <td style="border:0"><span style="color:red">{drive}</span>/{cblog}</td>
-</tr>
-%
-""".format(
-    img = car['img'].replace('960.jpg', '80.jpg').replace('480.jpg', '80.jpg'),    
-    name = car['name'],
-    url = car['url'],
-    drive = car['drive'],
-    cblog = car['cblog'],
-    former = 'style="color:gray"' if car['former'] == '1' else ''
-) for car in u['cars'])),
-    cmu = u'<ul>{}</ul>'.format(''.join(u"""
-<li><a target="_blank" href="{url}" {sadm}>{name}{nadm}</a></li>
-%
-""".format(
-    name = c['name'],
-    url = c['url'],
-    nadm = ' (adm)' if c['adm'] == '1' else '',
-    sadm = 'style="color:red"' if c['adm'] == '1' else '',
-) for c in u['communities'])),
-))
+        wdata(f, jnacpath, nacps)
+        wdata(f, jamopath, amops)
+        wdata(f, jliqpath, liqps)
         f.write(u'</table>')
-
-def ru(u):
-    return json.loads(asdbr('usrs.rzn.json/{}'.format(u)))
 
 # PAGES
 
-def hasnac(u):
-    u = ru(u)
-    cars = u''.join(c['name'] for c in u['cars']).lower()
-    return ('almera' in cars and 'classic' in cars)
-
-def hasgarage(u):
-    u = ru(u)
-    cars = u''.join(c['name'] for c in u['cars']).lower()
-    cmu = u''.join(c['name'] for c in u['communities']).lower()
-    data = u'{}{}{}{}'.format(u['descr'], u['about'], cars, cmu).lower()
-    return (
-            u'гараж' in data
-            or u'garag' in data
-            or u'гораж' in data
-            or u'gorag' in data
-    )
-
-def hasinterest(u):
-    u = ru(u)
-    cmu = u''.join(c['name'] for c in u['communities']).lower()
-    data = u'{}'.format(u['about']).lower()
-    datamax = u'{}{}'.format(u['about'], cmu).lower()
-    return (
-            u'youtube' in data
-            or u'ютуб' in data
-            or u'it' in data
-            or u'радио' in data
-            or u'спорт' in data
-            or u'кросс' in data
-            or u'работ' in data
-            or u'прода' in data
-            or u'кулибин' in data
-            or u'kulibin' in data
-            or u'сервис' in data
-            or u'custom' in data
-            or u'tuning' in data
-            or u'тюнинг' in data
-            or u'усовершенст' in data
-            or u'модерн' in data
-    )
-
-#aspage(
-#    'uflw',
-#    'Ryazan Users: Max Followers',
-#    srt = lambda us: us.sort(key = lambda u: int(ru(u)['counters']['followers']), reverse = True),
-##    fltlast = lambda us: us[:500],
-#)
-#
-#aspage(
-#    'ulast',
-#    'Ryazan Users: Last 50',
-#    srt = lambda us: us.sort(key = lambda u: datetime.datetime.strptime(ru(u)['rdate'], '%Y-%m-%d'), reverse = True),
-#    fltlast = lambda us: us[:50],
-#)
-#
-#aspage(
-#    'unac',
-#    'Ryazan Users: Nissan Almera Classic',
-#    fltfirst = lambda us: filter(lambda u: hasnac(u), us),
-#    srt = lambda us: us.sort(key = lambda u: int(ru(u)['counters']['followers']), reverse = True),
-#)
-#
-#aspage(
-#    'ugar',
-#    'Ryazan Users: Garage',
-#    fltfirst = lambda us: filter(lambda u: hasgarage(u), us),
-#    srt = lambda us: us.sort(key = lambda u: int(ru(u)['counters']['followers']), reverse = True),
-#)
-
-aspage(
-    'uint',
-    'Ryazan Users: Interesting',
-    fltfirst = lambda us: filter(lambda u: hasinterest(u), us),
-    srt = lambda us: us.sort(key = lambda u: int(ru(u)['counters']['followers']), reverse = True),
-)
+aspage('all', 'AlmeraShop: All Products')
